903683383810179fb587d97f73e501220c60b6fe
Luca Fischer <luca.fischer@btc-ag.com>
2024-09-23 07:40:16 +0200

    gesamten Code in Main zusammengefügt und gewählten Avatar auf dem Spielbrett geprinted

--- src/main/java/com/btcag/bootcamp/Main.java
+++ src/main/java/com/btcag/bootcamp/Main.java
@@ -1,7 +1,66 @@
 package com.btcag.bootcamp;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        // Intro
+
         System.out.println("Welcome to Robot Wars!!!");
+        System.out.println("      _____");
+        System.out.println("     /     \\");
+        System.out.println("    |  O O  |");
+        System.out.println("    |   ^   |");
+        System.out.println("     \\_____/");
+        System.out.println("      || ||");
+        System.out.println("      || ||");
+        System.out.println();
+
+
+        // Namenseingabe
+        Scanner inputName = new Scanner(System.in);
+        System.out.println("Bitte gib deinen gewünschten Benutzernamen ein");
+        String username = inputName.nextLine();
+
+        while (username.length() < 4 || username.length() > 16) {
+            System.out.println("Der eingegebene Name ist unzulässig.");
+            username = inputName.nextLine();
+        }
+        System.out.println("Willkommen zu ROBOT-WARS " + username + "!!!");
+
+
+        //Avatarerstellung
+        Scanner input_avatar = new Scanner(System.in);
+        System.out.println("Wähle ein Zeichen als Avatar");
+        String avatar = input_avatar.nextLine();
+
+        while(avatar.length() > 1) {
+            System.out.println("Dein Avatar darf nur ein Zeichen sein");
+            System.out.println("Bitte gib erneut ein Zeichen als Avatar ein");
+            avatar = input_avatar.nextLine();
+        }
+        System.out.println("Dein Avatar: " + avatar);
+
+
+        //Spielbretterstellung und erste Positionierung des Roboters.
+        int y = 0;
+        int x = 0;
+        int roboty = 7;
+        int robotx = 9;
+
+        while (y < 15) {
+            x = 0;
+            while (x < 15) {
+                if (x == robotx && y == roboty) {
+                    System.out.print(" [ " + avatar + " ] ");
+                } else {
+                    System.out.print(" [   ] ");
+                }
+                x++;
+            }
+            System.out.println();
+            System.out.println();
+            y++;
+        }
     }
 }
