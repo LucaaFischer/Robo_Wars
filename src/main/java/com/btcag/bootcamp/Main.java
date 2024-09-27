2f4b9f1f6c90883c4e7277b3e9a318c81f242ea5
Luca Fischer <luca.fischer@btc-ag.com>
2024-09-27 13:20:17 +0200

    Random Gewinner bestimmt. Gültigkeit der Züge überprüft, Avatarerstellung in Methode ausgelagert

--- src/main/java/com/btcag/bootcamp/Main.java
+++ src/main/java/com/btcag/bootcamp/Main.java
@@ -1,12 +1,20 @@
 package com.btcag.bootcamp;
 
+import java.util.Random;
 import java.util.Scanner;
 
+//---------------------------------------------------Deklaration von in unterschiedlichen Methoden gebrauchten Variablen---------------------------------------------------------------
 public class Main {
     public static Scanner input = new Scanner(System.in);
     public static String[] players = getPlayers();
     public static String[] avatars = getAvatars();
+    public static int maxTurns = 0;
+    public static int roboOneX = 0;
+    public static int roboOneY = 0;
+    public static int roboTwoX = 0;
+    public static int roboTwoY = 0;
 
+    //-----------------------------------------------------------------------Main-------------------------------------------------------------------------------------------------
     public static void main(String[] args) {
         String introString = intro();
         System.out.println(introString);
@@ -17,13 +25,13 @@ public class Main {
 
         System.out.println("Dein Avatar " + players[1] + ": " + avatars[1]);
 
-        board();
+        game();
     }
 
     //--------------------------------------------------------------------ASCII-Art Intro-------------------------------------------------------------------------------------
     public static String intro() {
         return (
-                        "  _____\n" +
+                "  _____\n" +
                         " /     \\\n" +
                         "|  O O  |\n" +
                         "|   ^   |\n" +
@@ -79,28 +87,26 @@ public class Main {
     }
 
     //----------------------------------------------------------Spielbretterstellung und Positionierung des Roboters.-----------------------------------------------------
-    public static void board() {
+    public static void game() {
         int y = 1;
         int x = 1;
-        int maxZuege = 0;
-        int roboOneX = 0;
-        int roboOneY = 0;
-        int roboTwoX = 0;
-        int roboTwoY = 0;
 
-        while (maxZuege <= 100) {
-            if (maxZuege % 2 == 0) {
+        while (maxTurns <= 100) {
+            if (maxTurns % 2 == 0) {
                 System.out.println("Wo möchtest du deinen Roboter platzieren, " + players[0] + "? x-Koordinate:");
                 roboOneX = input.nextInt();
                 System.out.println("Und die Y-Koordinate?");
                 roboOneY = input.nextInt();
+
             } else {
                 System.out.println("Wo möchtest du deinen Roboter platzieren, " + players[1] + "? x-Koordinate:");
                 roboTwoX = input.nextInt();
                 System.out.println("Und die Y-Koordinate?");
                 roboTwoY = input.nextInt();
             }
-            y = 0;
+            moveValid();
+
+            y = 1;
             while (y <= 15) {
                 x = 1;
                 while (x <= 15) {
@@ -117,10 +123,50 @@ public class Main {
                 System.out.println();
                 y++;
             }
-            System.out.println(players[0] + "'s Roboter (" + avatars[0] + ") befindet sich auf Feld x " + roboOneX + " y " + roboOneY + ".");
-            System.out.println(players[1] + "'s Roboter (" + avatars[1] + ") befindet sich auf Feld x " + roboTwoX + " y " + roboTwoY + ".");
-            maxZuege++;
+            if (roboOneX == roboTwoX && roboOneY == roboTwoY) {
+                System.out.println("AND THE WINNER ISSSSS " + fight_randWinner().toUpperCase());
+                break;
+            } else {
+                System.out.println(players[0] + "'s Roboter (" + avatars[0] + ") befindet sich auf Feld x " + roboOneX + " y " + roboOneY + ".");
+                System.out.println(players[1] + "'s Roboter (" + avatars[1] + ") befindet sich auf Feld x " + roboTwoX + " y " + roboTwoY + ".");
+                maxTurns++;
+            }
         }
     }
+
+    //-----------------------------------------------------------Überprüfung auf Möglichkeit des Zuges-------------------------------------------------------------------------
+    public static void moveValid() {
+        if (roboOneX > 15 || roboOneY > 15) {
+            System.out.println("Der gewünschte Zug befindet sich nicht auf der Karte. Bitte nochmal ziehen.");
+            System.out.println("Wo möchtest du deinen Roboter platzieren, " + players[0] + "? x-Koordinate:");
+            roboOneX = input.nextInt();
+            System.out.println("Und die Y-Koordinate?");
+            roboOneY = input.nextInt();
+            moveValid();
+        } else if (roboTwoX > 15 || roboTwoY > 15) {
+            System.out.println("Der gewünschte Zug befindet sich nicht auf der Karte. Bitte nochmal ziehen.");
+            System.out.println("Wo möchtest du deinen Roboter platzieren, " + players[1] + "? x-Koordinate:");
+            roboTwoX = input.nextInt();
+            System.out.println("Und die Y-Koordinate?");
+            roboTwoY = input.nextInt();
+            moveValid();
+        }
+    }
+
+    //----------------------------------------------------------------Zufälligen Gewinner bestimmen-------------------------------------------------------------------------
+    public static String fight_randWinner() {
+        String winner = "";
+        if (roboOneX == roboTwoX && roboOneY == roboTwoY) {
+            Random random = new Random();
+            int randWinner = random.nextInt(0, 2);
+            if (randWinner == 0) {
+                winner = players[0];
+            } else {
+                winner = players[1];
+            }
+        }
+        return winner;
+    }
 }
+
 
